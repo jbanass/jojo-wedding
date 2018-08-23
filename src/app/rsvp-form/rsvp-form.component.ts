c7d16924197ef033ccae7c5347e92db9e6a29178
Joseph Banass <joe@morsedata.com>
2018-08-22 21:15:28 -0500

    styling work

--- src/app/rsvp-form/rsvp-form.component.ts
+++ src/app/rsvp-form/rsvp-form.component.ts
@@ -7,13 +7,14 @@ import { RSVPService } from "./rsvp.service";
 @Component({
   selector: "rsvp-form",
   templateUrl: "./rsvp-form.component.html",
-  styleUrls: ["./rsvp-form.component.css"]
+  styleUrls: ["./rsvp-form.component.scss"]
 })
 export class RSVPFormComponent {
   validationCode: string = null;
   isValidated: boolean = false;
+  fg: FormGroup;
 
-  constructor(private service: RSVPService) {}
+  constructor(private service: RSVPService) { }
 
   validate(code: string) {
     this.service.submitRSVP(code).subscribe((result: boolean) => {
