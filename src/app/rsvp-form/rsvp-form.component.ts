f9a2250f6957ac5e9a284b52c3a3536ee6d1cff1
Joseph Banass <joe@morsedata.com>
2018-08-23 19:22:20 -0500

    dynamic form generation working, mobile layout styling

--- src/app/rsvp-form/rsvp-form.component.ts
+++ src/app/rsvp-form/rsvp-form.component.ts
@@ -1,6 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormBuilder, FormArray } from "@angular/forms";
 
 import { RSVPService } from "./rsvp.service";
 
@@ -9,12 +9,57 @@ import { RSVPService } from "./rsvp.service";
   templateUrl: "./rsvp-form.component.html",
   styleUrls: ["./rsvp-form.component.scss"]
 })
-export class RSVPFormComponent {
+export class RSVPFormComponent implements OnInit {
   validationCode: string = null;
   isValidated: boolean = false;
-  fg: FormGroup;
+  party: FormGroup;
+  partyList: FormGroup;
+  partyLeaderName: string = '';
 
-  constructor(private service: RSVPService) { }
+
+  constructor(private service: RSVPService, private fb: FormBuilder) { }
+
+  ngOnInit() {
+    // this.partyLeader = new FormGroup({
+    //   name: new FormControl(),
+    //   guestCount: new FormControl()
+    // });
+  }
+
+  buildForm(partyLeaderName: string, guestCount: string) {
+    if (partyLeaderName !== null && guestCount !== null) {
+      this.partyLeaderName = partyLeaderName;
+      var people = [];
+
+      const guestLimit = parseInt(guestCount) + 1;
+
+      for (var i = 0; i < guestLimit; i++) {
+        console.log(i);
+        people.push(this.createPerson(i));
+      }
+
+      this.partyList = this.fb.group({
+        people: this.fb.array(people)
+      });
+    }
+  }
+
+  createPerson(index: number) {
+    var name: string = '';
+    var food: number = null;
+    var foodNotes: string = '';
+
+    if (index === 0) {
+      name = this.partyLeaderName;
+      console.log("Hit index 0", name);
+    }
+
+    return this.fb.group({
+      name: name,
+      food: food,
+      foodNotes: foodNotes
+    });
+  }
 
   validate(code: string) {
     this.service.submitRSVP(code).subscribe((result: boolean) => {
