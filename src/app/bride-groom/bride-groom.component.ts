6d9969e09e981de6c14a67c88b002b0325ad2e95
Joseph Banass <jbanass@gmail.com>
2018-09-04 11:52:10 -0500

    Update bride-groom to remove animations component

--- src/app/bride-groom/bride-groom.component.ts
+++ src/app/bride-groom/bride-groom.component.ts
@@ -5,52 +5,9 @@ import { Subscription, Observable, fromEvent } from "rxjs";
 @Component({
   selector: "bride-groom",
   templateUrl: "./bride-groom.component.html",
-  styleUrls: ["./bride-groom.component.scss"],
-  animations: [
-    trigger('outOfView', [
-      state('outOfView', style({
-        transform: 'translateX(-100%)',
-        opacity: '0'
-      })),
-      state('inView', style({
-        transform: 'translatex(0)',
-        opacity: '1'
-      })),
-      transition('outOfView => inView', animate('0.5s 1s ease-out')),
-    ]),
-    trigger('brideInfoOutOfView', [
-      state('brideInfoOutOfView', style({
-        transform: 'translateX(-100%)',
-        opacity: '0'
-      })),
-      state('brideInfoInView', style({
-        transform: 'translatex(0)',
-        opacity: '1'
-      })),
-      transition("brideInfoOutOfView => brideInfoInView", animate('0.5s 1s ease-out'))
-    ]),
-    trigger('groomInfoOutOfView', [
-      state('groomInfoOutOfView', style({
-        transform: 'translateX(-100%)',
-        opacity: '0'
-      })),
-      state('groomInfoInView', style({
-        transform: 'translatex(0)',
-        opacity: '1'
-      })),
-      transition("groomInfoOutOfView => groomInfoInView", animate('0.5s 1s ease-out'))
-    ])
-  ]
+  styleUrls: ["./bride-groom.component.scss"]
 })
 export class BrideGroomComponent {
-  @ViewChild('top') topOfComponent: ElementRef;
-  containerIsInView: string = 'outOfView';
-
-  @ViewChild('bride') brideElement: ElementRef;
-  brideIsInView: string = 'brideInfoOutOfView';
-
-  @ViewChild('groom') groomElement: ElementRef;
-  groomIsInView: string = 'groomInfoOutOfView';
 
   scrollPos: number;
   windowHeight: number;
@@ -62,34 +19,6 @@ export class BrideGroomComponent {
   }
 
   ngAfterViewInit() {
-    this.subscriptionScroll = fromEvent(window, 'scroll').subscribe(() => this.onScroll());
+    
   }
-
-  checkVisibility() {
-    if (this.scrollPos >= (<HTMLDivElement>this.topOfComponent.nativeElement).getBoundingClientRect().top) {
-      if (this.containerIsInView !== 'inView') {
-        this.containerIsInView = 'inView';
-      }
-    }
-
-    if (this.scrollPos >= (<HTMLDivElement>this.brideElement.nativeElement).getBoundingClientRect().top) {
-      if (this.brideIsInView !== 'brideInfoInView') {
-        this.brideIsInView = 'brideInfoInView';
-      }
-    }
-
-    if (this.scrollPos >= (<HTMLDivElement>this.groomElement.nativeElement).getBoundingClientRect().top) {
-      if (this.groomIsInView !== 'groomInfoInView') {
-        this.groomIsInView = 'groomInfoInView';
-      }
-    }
-  }
-
-  onScroll() {
-    this.scrollPos = window.scrollY;
-    this.windowHeight = window.innerHeight;
-    this.checkVisibility();
-  }
-
-
 }
